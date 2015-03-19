89ce34416fd53653ee97e9d5cdd04263b4007f25
Weih Li <weihotline@gmail.com>
2015-03-19 13:08:58 -0700

    typo

--- lib/HeapSort.swift
+++ lib/HeapSort.swift
@@ -3,7 +3,7 @@ import Foundation
 // HeapSort
 class HeapSort<T: Comparable> {
     // time complexity: O(nlogn)
-    // space complexity: O(1) in-place, not extract stackframe
+    // space complexity: O(1) in-place, not extra stackframe
     class func sort(var array: Array<T>) -> Array<T> {
         let count = array.count
         if count < 2 { return array }
